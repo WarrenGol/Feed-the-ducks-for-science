2cef1b5e2787f549116d16ec97194c7927cc27dc
warren <warren.golin@gmail.com>
2018-10-28 18:23:27 -0700

    need to have the proper variable name to trigger auth in mongo

--- mongo/init_users.sh
+++ mongo/init_users.sh
@@ -1,4 +1,4 @@
 #/usr/bin/bash
 
-mongo admin --host localhost --port 27017 -u $MONGO_INIT_USERNAME -p $MONGO_INIT_PASSWORD --eval "
+mongo admin --host localhost --port 27017 -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval "
 db.createUser({user: '$DB_USERNAME', pwd:'$DB_PASSWORD', roles:[{role: 'readWrite', db:'website'}]});"
